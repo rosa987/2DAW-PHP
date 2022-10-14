00abdbd31b80962e13555b8027bc818e7a640599
rosa987 <113983980+rosa987@users.noreply.github.com>
2022-10-14 12:58:06 +0200

    Add files via upload

---
+++ forms/binario_ver2.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>CONVERSOR BINARIO</title>
+</head>
+<body>
+<h2>CONVERSOR BINARIO</h2>
+<p>Resultado operación: 
+
+<?php
+$decimal ="";
+$decimal =  test_input($_POST['decimal']);
+function DecimalAbinario($decimal)
+{
+    return $binario=decbin($decimal);
+}
+
+//Llamo a la funcion
+echo "El numero decimal ". $decimal." en binario es: ";
+echo DecimalAbinario($decimal);
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+?>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
+ <label for="decimal">Numero Decimal:</label>
+  <input type="text" id="decimal" name="decimal" value="<?php echo htmlspecialchars($decimal); ?>" ><br>
+  <br>
+  <label for="binario">Numero Binario:</label>
+  <input type="text" id="binario" name="binario" value="<?php echo htmlspecialchars(DecimalAbinario($decimal)); ?>" ><br>
+  <br>
+</form>
+</p> 
+</body>
+</html>
