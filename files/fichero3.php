f6232ca15e8d43397e3f9a53fdb6414c217b9b55
rosa987 <113983980+rosa987@users.noreply.github.com>
2022-10-26 10:36:02 +0200

    Add files via upload

--- files/fichero3.php
+++ files/fichero3.php
@@ -2,7 +2,6 @@
 echo "<table border='1'; style='border-collapse:collapse'>";
 
 $file = fopen("alumnos1.txt", "r") or die("Unable to open file!");
-$filePath="alumnos1.txt";
 $contarLineas = 0;
 while (!feof($file)){   
     $datos = fgets($file); 
