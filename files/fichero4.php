931361895f0c0b898c4d39eda9e61f06149d7eef
rosa987 <113983980+rosa987@users.noreply.github.com>
2022-10-26 12:57:54 +0200

    Add files via upload

---
+++ files/fichero4.php
@@ -0,0 +1,27 @@
+<?php
+echo "<table border='1'; style='border-collapse:collapse'>";
+//$arrayDatos=array($nombre,$apellido1, $apellido2,$fnacimiento, $localidad );
+
+$file = fopen("alumnos2.txt", "r") or die("Unable to open file!");
+$contarLineas = 0;
+
+while (!feof($file)){   
+    $datos = fgets($file); 
+		
+	list($nombre, $apellido1, $apellido2, $fnacimiento, $localidad) = explode("##", $datos);
+	
+    echo "<tr>";
+	echo "<td>" . $nombre. "</td>";
+	echo "<td>" . $apellido1. "</td>";
+	echo "<td>" . $apellido2. "</td>";
+	echo "<td>" . $fnacimiento. "</td>";
+	echo "<td>" . $localidad. "</td>";
+	echo "</tr>";
+	$contarLineas++;
+}
+echo '</table>';
+//$contarLineas=$contarLineas-1;
+fclose($file);
+ echo "El número de filas que se han leído son: ".$contarLineas;
+    
+?>
